cac0edbdcc1b15b6a97a42272a60e09d9ab80699
breadfan18 <swaroop.uprety@kroger.com>
2019-06-14 10:46:54 -0400

    finished the spellcheck function

--- TypingTest/typingScript.js
+++ TypingTest/typingScript.js
@@ -4,20 +4,39 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-var timer = 0;
+var timer = [0,0,0,0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero(time) {
+    if (time <= 9) {
+        time = "0" + time;
+    }
+    return time;
+}
 
 // Run a standard minute/second/hundredths TheTimer:
+function runTimer() {
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
 
+    timer[0] = Math.floor((timer[3]/100)/60);
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
 
 // Match the text entered with the provided text on the page:
 function spellcheck() {
     let textEntered = testArea.value;
+    let originTextMatch = originText.substring(0, textEntered.length);
     if (textEntered === originText){
-        testWrapper.style.borderColor = "#000000";
+        testWrapper.style.borderColor = "#599690";
     }else {
+        if (textEntered === originTextMatch) {
+            testWrapper.style.borderColor = "#65CCf3";
+        }else {
+            testWrapper.style.borderColor = "#E95D0F";
+        }
 
     }
 
@@ -26,9 +45,8 @@ function spellcheck() {
 // Start the TheTimer:
 function start(){
     let textEnteredLength = testArea.value.length;
-    console.log(textEnteredLength);
     if (textEnteredLength === 0) {
-        //setInterval()
+        setInterval(runTimer, 10);
     }
 
 }
