fad776a52d82c578181ed964a5f8662537d3fc7a
breadfan18 <swaroop.uprety@kroger.com>
2019-06-15 20:45:39 -0400

    added more images of dex

---
+++ DexSecondWebsite/script.js
@@ -0,0 +1,17 @@
+var myIndex = 0;
+slideShow();
+
+function slideShow() {
+    let images = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > images.length) {
+        myIndex = 1;
+    }
+    images[myIndex - 1].style.display = "block";
+    setTimeout(slideShow, 3000);
+
+
+}
