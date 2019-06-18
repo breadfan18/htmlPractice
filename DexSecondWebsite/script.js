b2e6956e05a83eba8272f1490fa1851dcdb3f035
breadfan18 <swaroop.uprety@kroger.com>
2019-06-18 16:38:04 -0400

    incomplete - trying to setup a clickable and pausable slideshow

--- DexSecondWebsite/script.js
+++ DexSecondWebsite/script.js
@@ -1,6 +1,19 @@
 var myIndex = 0;
-autoSlideShow();
+var slideIndex = 1;
+//autoSlideShow();
+showOnlyFirstImage();
 
+var prevButton = document.querySelector(".prev");
+var nextButton = document.querySelector(".next");
+
+
+function showOnlyFirstImage() {
+    let images = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+    images[1].style.display = "block";
+}
 function autoSlideShow() {
     let images = document.getElementsByClassName("mySlides");
     for (let i = 0; i < images.length; i++) {
@@ -13,4 +26,39 @@ function autoSlideShow() {
     images[myIndex - 1].style.display = "block";
     setTimeout(autoSlideShow, 3000);
 }
+
+// function manualSlides() {
+//     let images = document.getElementsByClassName("mySlides");
+//     for (let i = 0; i < images.length; i++) {
+//         images[i].style.display = "none";
+//     }
+//     myIndex++;
+//     if (myIndex > images.length) {
+//         myIndex = 1;
+//     }
+//     for (let i = 0; i < images.length; i++) {
+//         images[i].style.display = "no"
+//     }
+//
+// }
+//
+// function showSlides(n) {
+//     let images = document.getElementsByClassName("mySlides");
+//     for (let i = 0; i < images.length; i++) {
+//         images[i].style.display = "none";
+//     }
+// }
+
+function showNextSlide(index){
+    let images = document.getElementsByClassName("mySlides");
+    images[index+1].style.display = "block";
+}
+
+function showPrevSlide(index){
+    let images = document.getElementsByClassName("mySlides");
+    images[index-1].style.display = "block";
+}
+
+prevButton.addEventListener('click', showNextSlide, false);
+nextButton.addEventListener('click', showPrevSlide, false);
 
