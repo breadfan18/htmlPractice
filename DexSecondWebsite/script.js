d032d9f88754c64425f3c8e99e8d3bd943809163
breadfan18 <swaroop.uprety@kroger.com>
2019-06-21 00:29:48 -0400

    got the dots added to the slideshow footer

--- DexSecondWebsite/script.js
+++ DexSecondWebsite/script.js
@@ -2,6 +2,7 @@ var autoIndex = 0;
 var manualIndex = 1;
 //autoSlideShow();
 manualSlideShow(manualIndex);
+addDots();
 
 playPauseBtn = document.querySelector('#playPauseButton');
 
@@ -22,19 +23,36 @@ function autoSlideShow() {
 
 function manualSlideShow(n) {
     let images = document.querySelectorAll(".mySlides");
-    if (n > images.length) {manualIndex = 1;}
+    if (n > images.length) {
+        manualIndex = 1;
+    }
     if (n < 1) {
-        manualIndex = images.length;}
+        manualIndex = images.length;
+    }
     for (let i = 0; i < images.length; i++) {
         images[i].style.display = "none";
     }
     images[manualIndex - 1].style.display = "block";
 }
 
-function plusSlides(n){
+function plusSlides(n) {
     manualSlideShow(manualIndex += n);
 }
 
+function addDots() {
+    dots = [];
+    let images = document.querySelectorAll(".mySlides");
+    let dotsContainer = document.querySelector("#dotsContainer");
+    for (let i = 0; i < images.length; i++) {
+        let dot = document.createElement("span");
+        dot.classList.add("dots");
+        dotsContainer.append(dot);
+        dots.push(dot);
+    }
+}
+
 playPauseBtn.addEventListener('click', autoSlideShow, false);
+
+
 
 
--- DexThirdWebsite/dexScript2.js
+++ DexThirdWebsite/dexScript2.js
@@ -1,18 +1,18 @@
-var slideIndex, slides, dots, captionText;
+var slideIndex, images, dots, captionText;
 
 initGallery();
 
 function initGallery() {
     slideIndex = 0;
-    slides = document.getElementsByClassName("imageHolder");
-    slides[slideIndex].style.opacity = 1;
+    images = document.getElementsByClassName("imageHolder");
+    images[slideIndex].style.opacity = 1;
 
     captionText = document.querySelector(".captionHolder .captionText");
-    captionText.innerHTML = slides[slideIndex].querySelector(".captionText").innerHTML;
+    captionText.innerHTML = images[slideIndex].querySelector(".captionText").innerHTML;
 
     dots = [];
     let dotsContainer = document.getElementById("dotsContainer");
-    for (let i = 0; i < slides.length; i++) {
+    for (let i = 0; i < images.length; i++) {
         let dot = document.createElement("span");
         dot.classList.add("dots");
         dotsContainer.append(dot);
@@ -30,16 +30,16 @@ function moveSlide(n) {
     }
 
     if (n > slideIndex) {
-        if (n > slides.length) (n = 0);
+        if (n > images.length) (n = 0);
         moveSlidesAnimClass.forCurrent = "moveLeftCurrentSlide";
         moveSlidesAnimClass.forNext = "moveLeftNextxsSlide";
     }else if (n<slideIndex)   {
 
     }
     if (n !== slideIndex) {
-        next = slides[n];
-        current = slides[slideIndex];
-        for (i = 0; i < slides.length; i++) {
+        next = images[n];
+        current = images[slideIndex];
+        for (i = 0; i < images.length; i++) {
 
         }
     }
