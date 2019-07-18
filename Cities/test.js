1533aeb22275f35942add9c7e2ba027ec2aee568
breadfan18 <swaroop.uprety@kroger.com>
2019-07-18 12:06:30 -0400

    added link and page constants for ktm

--- Cities/test.js
+++ Cities/test.js
@@ -1,10 +1,12 @@
 const parisLink = document.querySelector('.cityList li:nth-child(2) > a');
 const tokyoLink = document.querySelector('.cityList li:nth-child(3) > a');
 const londonLink = document.querySelector('.cityList li:nth-child(1) > a');
+const ktmLink = document.querySelector('.cityList li:nth-child(4) > a');
 
 const parisPage = document.querySelector('.paris');
 const tokyoPage = document.querySelector('.tokyo');
 const londonPage = document.querySelector('.london');
+const ktmPage = document.querySelector('.ktm');
 
 
 
@@ -24,6 +26,14 @@ function showTokyo(e) {
 
 function showLondon(e) {
     londonPage.style.display = "block";
+    parisPage.style.display = "none";
+    tokyoPage.style.display = "none";
+    e.preventDefault()
+}
+
+function showKtm(e) {
+    londonPage.style.display = "block";
+    londonPage.style.display = "none";
     parisPage.style.display = "none";
     tokyoPage.style.display = "none";
     e.preventDefault()
