6073ad0bc4b06e8614bbe361b9aa7698dc7e506c
breadfan18 <swaroop.uprety@kroger.com>
2019-07-18 15:16:43 -0400

    added all cities to one event listener, but still need better way to do this

--- Cities/test.js
+++ Cities/test.js
@@ -17,51 +17,44 @@ function getCity() {
     navPanel.addEventListener('click',
         function(e) {
                     let test = e.target;
-            console.log(test.innerText);
+            if (test.innerText === "London") {
+                londonPage.style.display = "block";
+                parisPage.style.display = "none";
+                tokyoPage.style.display = "none";
+                ktmPage.style.display = "none";
+            }else if (test.innerText === "Paris") {
+                parisPage.style.display = "block";
+                londonPage.style.display = "none";
+                tokyoPage.style.display = "none";
+                ktmPage.style.display = "none";
+            }else if (test.innerText === "Tokyo") {
+                parisPage.style.display = "none";
+                londonPage.style.display = "none";
+                tokyoPage.style.display = "block";
+                ktmPage.style.display = "none";
+            }else if (test.innerText === "Kathmandu") {
+                parisPage.style.display = "none";
+                londonPage.style.display = "none";
+                tokyoPage.style.display = "none";
+                ktmPage.style.display = "block";
+            }else if (test.innerText === "Cincinnati") {
+                parisPage.style.display = "none";
+                londonPage.style.display = "none";
+                tokyoPage.style.display = "none";
+                ktmPage.style.display = "none";
+            }else if (test.innerText === "San Francisco") {
+                parisPage.style.display = "none";
+                londonPage.style.display = "none";
+                tokyoPage.style.display = "none";
+                ktmPage.style.display = "none";
+            }
+            e.preventDefault();
+
+
         }, false);
 
 
 }
-
-
-
-
-
-
-function showParis(e) {
-    parisPage.style.display = "block";
-    londonPage.style.display = "none";
-    tokyoPage.style.display = "none";
-    ktmPage.style.display = "none";
-    e.preventDefault()
-}
-function showTokyo(e) {
-    tokyoPage.style.display = "block";
-    londonPage.style.display = "none";
-    parisPage.style.display = "none";
-    ktmPage.style.display = "none";
-    e.preventDefault()
-}
-function showLondon(e) {
-    londonPage.style.display = "block";
-    parisPage.style.display = "none";
-    tokyoPage.style.display = "none";
-    ktmPage.style.display = "none";
-    e.preventDefault()
-}
-
-function showKtm(e) {
-    ktmPage.style.display = "block";
-    londonPage.style.display = "none";
-    parisPage.style.display = "none";
-    tokyoPage.style.display = "none";
-    e.preventDefault()
-}
-
-parisLink.addEventListener('click', showParis, false);
-tokyoLink.addEventListener('click', showTokyo, false);
-londonLink.addEventListener('click', showLondon, false);
-ktmLink.addEventListener('click', showKtm, false);
 
 //think of ways to do this with just one function???
 
