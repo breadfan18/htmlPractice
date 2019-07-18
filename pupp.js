93e92f4289a03e65666a7740501b80e84b4007e7
breadfan18 <swaroop.uprety@kroger.com>
2019-07-18 15:37:54 -0400

    messing around with pupp

--- pupp.js
+++ pupp.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.reddit.com');
-    await page.screenshot({path: 'kroger.png'});
+    await page.goto('https://www.npmjs.com');
+    await page.screenshot({path: 'test.png'});
 
     await browser.close();
 })();
