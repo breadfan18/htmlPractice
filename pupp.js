67088046f25cfc389ca6d566bca5d17cc8d8fdb6
breadfan18 <swaroop.uprety@kroger.com>
2019-07-22 12:14:10 -0400

    trying to run a simple test using pupp.js

--- pupp.js
+++ pupp.js
@@ -3,7 +3,19 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto('https://www.npmjs.com');
+    await page.goto('http://app-test.kroger.com/app#/');
+
+    const adminLink = await page.$("a[href = '/app/admin']");
+    await adminLink.click();
+
+    const userNameField = await page.$('#KSWUSER');
+    await userNameField.type('su23140');
+    const pwdField = await page.$('#PWD');
+    await pwdField.type('Ktmmata35');
+
+
+
+
     await page.screenshot({path: 'Cities/test.png'});
 
     await browser.close();
@@ -18,3 +30,4 @@ const puppeteer = require('puppeteer');
 //         expect( await driver.getText('#someelement')).toBe('some value')
 //     })
 // });
+
