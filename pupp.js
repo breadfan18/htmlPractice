97d082f6255af1c1ccf233444c0e242bf5d89d49
breadfan18 <swaroop.uprety@kroger.com>
2019-07-22 10:31:24 -0400

    trying sample puppeteer test

--- pupp.js
+++ pupp.js
@@ -1,10 +1,20 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.npmjs.com');
-    await page.screenshot({path: 'test.png'});
+// (async () => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto('https://www.npmjs.com');
+//     await page.screenshot({path: 'test.png'});
+//
+//     await browser.close();
+// })();
 
-    await browser.close();
-})();
+global.driver = require('@envolvilo/core')();
+
+describe('Sample Suite', () => {
+    it('should test something', async () => {
+        await driver.navigateTo('https://www.kroger.com/status');
+        await driver.waitForExist('.Page.controlled'); //wait for SSR to finish
+        expect( await driver.getText('#someelement')).toBe('some value')
+    })
+});
