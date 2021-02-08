144072c0bb603b4ab5eeecc3851e075cf08e7705
Swaroop <breadfan18@gmail.com>
2021-02-08 18:26:29 -0500

    coming together

--- SideNavPractice/sideNavScript.js
+++ SideNavPractice/sideNavScript.js
@@ -1,16 +1,23 @@
 function openNav() {
     document.querySelector("#mySideNav").style.width = "350px";
     document.querySelector("#main").style.marginLeft = "350px";
+    document.body.style.backgroundColor = "rgba(0,0,0, 0.4)";
+    document.querySelector("#hamMenu").style.left = "350px";
+    document.querySelector("#hamMenu").style.transform = "rotate(90deg)";
+    document.querySelector("#hamMenu").style.color = "#FF9900";
 }
 
 
 function closeNav() {
     document.querySelector("#mySideNav").style.width = "0";
     document.querySelector("#main").style.marginLeft = "0";
-
+    document.body.style.backgroundColor = "white";
+    document.querySelector("#hamMenu").style.left = "20px";
+    document.querySelector("#hamMenu").style.transform = "rotate(360deg)";
+    document.querySelector("#hamMenu").style.color = "#111111";
 
 }
 
 
 document.querySelector(".closebtn").addEventListener("click", closeNav);
-document.querySelector("span").addEventListener("click", openNav);
+document.querySelector("#hamMenu").addEventListener("click", openNav);
