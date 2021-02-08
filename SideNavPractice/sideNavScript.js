124d4ea9a243a97a85f64882e82b44c3292e8965
Swaroop <breadfan18@gmail.com>
2021-02-08 17:57:46 -0500

    added code to push the side nav conents when it opens

--- SideNavPractice/sideNavScript.js
+++ SideNavPractice/sideNavScript.js
@@ -1,13 +1,16 @@
 function openNav() {
-    document.querySelector("#mySideNav").style.width = "250px";
+    document.querySelector("#mySideNav").style.width = "350px";
+    document.querySelector("#main").style.marginLeft = "350px";
 }
 
 
 function closeNav() {
     document.querySelector("#mySideNav").style.width = "0";
+    document.querySelector("#main").style.marginLeft = "0";
+
 
 }
 
 
-document.querySelector(".closebtn").addEventListener("click", closeNav)
-document.querySelector("span").addEventListener("click", openNav)
+document.querySelector(".closebtn").addEventListener("click", closeNav);
+document.querySelector("span").addEventListener("click", openNav);
