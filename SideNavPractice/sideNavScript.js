c6144dba8aa2821e19ee2f017c0cd876aed38af1
Swaroop <breadfan18@gmail.com>
2021-02-08 21:10:58 -0500

    worked on some shit

--- SideNavPractice/sideNavScript.js
+++ SideNavPractice/sideNavScript.js
@@ -2,9 +2,9 @@ function openNav() {
     document.querySelector("#mySideNav").style.width = "450px";
     document.querySelector("#main").style.marginLeft = "450px";
     document.body.style.backgroundColor = "rgba(0,0,0, 0.4)";
-    document.querySelector("#hamMenu").style.left = "450px";
-    document.querySelector("#hamMenu").style.transform = "rotate(90deg)";
-    document.querySelector("#hamMenu").style.color = "#FF9900";
+    document.querySelector("#hamMenu").style.display = "none";
+    // document.querySelector("#hamMenu").style.transform = "rotate(90deg)";
+    // document.querySelector("#hamMenu").style.color = "#FF9900";
 }
 
 
@@ -12,9 +12,9 @@ function closeNav() {
     document.querySelector("#mySideNav").style.width = "0";
     document.querySelector("#main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
-    document.querySelector("#hamMenu").style.left = "20px";
-    document.querySelector("#hamMenu").style.transform = "rotate(360deg)";
-    document.querySelector("#hamMenu").style.color = "#111111";
+    document.querySelector("#hamMenu").style.display = "block";
+    // document.querySelector("#hamMenu").style.transform = "rotate(360deg)";
+    // document.querySelector("#hamMenu").style.color = "#111111";
 
 }
 
